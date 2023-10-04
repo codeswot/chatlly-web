86dd26a5b2f450f7b5a590b7a381c8d6947ac6e5
IBRAHIM MUHAMMAD <bibcodes5@gmail.com>
2023-10-04 19:01:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import SideBar from './sideBar';
-import Users from './users'
+import Users from './users';
+import MessageBox from './messageBox';
 
 function App() {
   return (
     <div className="App">
       <SideBar/>
       <Users/>
+      <MessageBox/>
     </div>
   );
 }
--- src/messageBox.js
+++ src/messageBox.js
@@ -1,9 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const MessageBox = () => {
     return (
         <div className="message-box">
             <input placeholder="Message" className="input" type="text" />
-            <FontAwesomeIcon icon="fa-sharp fa-solid fa-paper-plane-top" />
         </div>
     );
 }
