3e81a2d4d113ae7d3e7b2853fc5c55b36f632f59
IBRAHIM MUHAMMAD <bibcodes5@gmail.com>
2023-10-05 17:15:38 +0100

    add login page

---
+++ src/register.js
@@ -0,0 +1,12 @@
+const Register = () => {
+    return (
+        <div className="register">
+            <h1>Register</h1>
+            <textarea cols="30" rows="1" placeholder="Email"></textarea>
+            <textarea cols="30" rows="1" placeholder="Password"></textarea>
+            
+        </div>
+    );
+}
+ 
+export default Register;
