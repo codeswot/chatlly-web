fdcf833e471ae5f86db548abb7f1a3b7790fa89d
Mubarak Ibrahim <40618838+codeswot@users.noreply.github.com>
2023-10-08 16:37:08 +0100

    fix the TODO

--- src/App.js
+++ src/App.js
@@ -9,15 +9,21 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Routes>
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-        </Routes>
-        <div className="body">
+        <Routes>
+          {/* <div className="body">
         <SideBar />
         <Users />
         <MessageBox />
-        </div>
+        </div> 
+        TODO: Make the above a seperate as (Chat) component and use it as the
+        element bello
+        */}
+        {/* also let the base path / route to Register */}
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path='/chat' element={<Chat />} /> */}
+        </Routes>
       </Router>
     </div>
   );
