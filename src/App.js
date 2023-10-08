c13eae2ba652367beb3d8a92543022974e305001
IBRAHIM MUHAMMAD <bibcodes5@gmail.com>
2023-10-08 18:53:13 +0100

    commit changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Router>
       <Routes>
-            <Route path='/login' element={<Login />} />
+            <Route path='/' element={<Login />} />
             <Route path='/register' element={<Register />} />
         </Routes>
         <div className="body">
