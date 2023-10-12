d34561326516e685117163a588a965ba74a76cf5
IBRAHIM MUHAMMAD <bibcodes5@gmail.com>
2023-10-12 17:22:58 +0100

    add send icon

--- src/messageBox.js
+++ src/messageBox.js
@@ -1,13 +1,19 @@
+import { IoSend } from 'react-icons/io5'
+
 const MessageBox = () => {
     return (
         <div className="message-box">
             <header>
-            <div className="user-two">
+                <div className="user-two">
                     <img src={require("./Screenshot from 2023-10-04 20-17-36.png")} alt="khaliel" />
                     <p id="user-id">owner</p>
                 </div>
             </header>
-            <textarea placeholder="Message" className="input" type="text" />
+            <div className="message">
+                <textarea placeholder="Message" className="input" type="text" />
+                <IoSend className='icon' />
+            </div>
+
         </div>
     );
 }
